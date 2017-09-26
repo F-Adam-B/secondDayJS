3165af4d9c5e08a1cb57d9f124119fef4d5166b2
Quang Nguyen <qtn2@njit.edu>
2017-09-26 13:18:29 -0400

    problem 2

--- loopArray.js
+++ loopArray.js
@@ -1,38 +1,65 @@
 // Max and Min Redo
 'use strict';
 
-function max(numbers) {
-  let largest = numbers[0];
-  if (numbers === []) {
-    return null;
-  }
-  else {
-    let i = 0;
-    while (i < numbers.length) {
-      if (numbers[i] >= largest) {
-        largest = numbers[i];
-      }
-      i++;
-    }
+// function max(numbers) {
+//   let largest = numbers[0];
+//   if (numbers === []) {
+//     return null;
+//   }
+//   else {
+//     let i = 0;
+//     while (i < numbers.length) {
+//       if (numbers[i] >= largest) {
+//         largest = numbers[i];
+//       }
+//       i++;
+//     }
     
-  }
-  return largest;
+//   }
+//   return largest;
+// }
+
+// function min(numbers) {
+//   let smallest = numbers[0];
+//   if (numbers === []) {
+//     return null;
+//   }
+//   else {
+//     let i = 0;
+//     while (i < numbers.length) {
+//       if (numbers[i] <= smallest) {
+//         smallest = numbers[i];
+//       }
+//       i++;
+//     }
+      
+//   }
+//   return smallest;
+// }
+
+Average redo using while loop
+function average(numbers) {
+  let total = numbers[0];
+  let i = 1;
+  while (i < numbers.length) {
+    total += numbers[i];
+    i++;
+  } 
+  return total/numbers.length;
+} 
+
+//Using forEach method
+function average(numbers) {
+  let sum = 0;
+  numbers.forEach(function(item) {
+    
+    sum += item;
+  });
+  return sum/numbers.length;
 }
 
-function min(numbers) {
-  let smallest = numbers[0];
-  if (numbers === []) {
-    return null;
-  }
-  else {
-    let i = 0;
-    while (i < numbers.length) {
-      if (numbers[i] <= smallest) {
-        smallest = numbers[i];
-      }
-      i++;
-    }
-      
-  }
-  return smallest;
-}
+average([1,2,3]);
+
+
+
+  
